5e379416c1e87bfdd840c7d6a8287342d8e223b9
melquetrindade <melquetrindade654@gmail.com>
2023-01-05 11:40:22 -0300

    iniciando a construção do site sig_academia

--- sig_academia/script.js
+++ sig_academia/script.js
@@ -1,73 +1,21 @@
-let numtxt = document.querySelector('input#numtxt')
-var sort = Math.floor(Math.random() * (100 - 0 + 1)) + 0
-let res = document.querySelector('div#res')
-let lista = document.querySelector('select#lista')
-let valores = []
-
-const resul = document.createElement("p")
-resul.setAttribute('id', 'resul')
-const texto = document.createTextNode(``)
-resul.appendChild(texto)
-res.appendChild(resul)
-var maior = 100
-var menor = 0
-var cont = 1
-
-function valNum(n){
-    if(n > maior || n < menor){
-        return false
-    }
-    else{
-        return true
-    }
+//Para a div Download
+var a = window.document.getElementById('area')
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+function entrar(){
+    a.style.background = 'green'
+}
+function sair(){
+    a.style.background = 'dodgerblue'
 }
 
-function inLista(l, n){
-    if(l.indexOf(Number(n)) == -1){
-        return true
-    }
-    else{
-        return false
-    }
+//Para a div home
+let home = window.document.querySelector('div#area2')
+home.addEventListener('mouseenter', entrarHome)
+home.addEventListener('mouseout', sairHome)
+function entrarHome(){
+    home.style.background = 'rgb(201, 201, 201)'
 }
-
-function verificar(){
-    if(valNum(numtxt.value) && inLista(valores, numtxt.value)){
-        valores.push(Number(numtxt.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${numtxt.value} adiionado`
-        lista.appendChild(item)
-        res.innerHTML = ''
-        var acertou = false
-        var sorteado = numtxt.value
-        if(sort == (Number(sorteado))){
-            acertou = true
-        }
-        else{
-            if(sort > Number(numtxt.value)){
-                menor = Number(numtxt.value) + 1
-            }
-            else{
-                maior = Number(numtxt.value) - 1
-            }
-            res.innerHTML += `DICA: ESCOLHA UM NÚMERO ENTRE ${menor} --- ${maior}`
-            cont += 1
-            numtxt.value = ''
-            numtxt.focus()
-        }
-        if(acertou == true || cont > 10){
-            if(acertou == true){
-                res.innerHTML += 'PARABÊNS VOCÊ ACERTOU O NÚMERO!<br>'
-                res.innerHTML += `TOTAL DE TENTATIVAS: ${cont}`
-            }
-            else{
-                res.innerHTML += 'INFELIZMENTE VOCÊ GASTOU TODAS AS SUAS CHANCES. GAME OVER!'
-            }
-        }
-    }
-    else{
-        window.alert('[ERRO] -> Número está fora dos limites ou já foi escolhido')
-        numtxt.value = ''
-        numtxt.focus()
-    }
+function sairHome(){
+    home.style.background = 'white'
 }
