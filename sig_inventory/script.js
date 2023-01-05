5e184a9ceeafac66693c62626d78d300f2efb77d
melquetrindade <melquetrindade654@gmail.com>
2023-01-05 11:08:22 -0300

    finalizando o site sig_inventory

--- sig_inventory/script.js
+++ sig_inventory/script.js
@@ -1,3 +1,4 @@
+//Para a div Download
 var a = window.document.getElementById('area')
 a.addEventListener('mouseenter', entrar)
 a.addEventListener('mouseout', sair)
@@ -6,4 +7,15 @@ function entrar(){
 }
 function sair(){
     a.style.background = 'dodgerblue'
+}
+
+//Para a div home
+let home = window.document.querySelector('div#area2')
+home.addEventListener('mouseenter', entrarHome)
+home.addEventListener('mouseout', sairHome)
+function entrarHome(){
+    home.style.background = 'rgb(201, 201, 201)'
+}
+function sairHome(){
+    home.style.background = 'white'
 }
