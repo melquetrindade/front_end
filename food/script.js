dee43787baee8e2d1f7b1f4646ba03d8ebd5b5cf
melquetrindade <melquetrindade654@gmail.com>
2023-02-12 11:43:40 -0300

    criando o script para mudar as seções de alimentos

--- food/script.js
+++ food/script.js
@@ -1,27 +1,101 @@
 let imenu = window.document.getElementById('op')
 let menu = window.document.getElementById('fundoMenu')
 let icone2 = window.document.querySelector('p.icone')
+let secHam = window.document.getElementById('hamburguer')
+let secPizza = window.document.getElementById('pizza')
+let secBebidas = window.document.getElementById('bebidas')
+let secSorvetes = window.document.getElementById('sorvetes')
 let i1 = window.document.getElementById('img1')
 let i2 = window.document.getElementById('img2')
 let i3 = window.document.getElementById('img3')
 let i4 = window.document.getElementById('img4')
 
-imenu.addEventListener('click', clicou)
+secHam.style.display = 'block'
+secPizza.style.display = 'none'
+secBebidas.style.display = 'none'
+secSorvetes.style.display = 'none'
+
+menu.style.display = 'none'
 
 function clicou(){
-        menu.style.backgroundColor = 'red'
-        i1.style.display = 'inline-block'
-        i2.style.display = 'inline-block'
-        i3.style.display = 'inline-block'
-        i4.style.display = 'inline-block'
+        if(fundoMenu.style.display == 'none'){
+                imenu.style.backgroundColor = 'red'
+                imenu.style.borderRadius = '50%'
+                imenu.style.padding = '5px'
 
-        menu.style.position = 'absolute'
-        menu.style.width = '100px'
-        menu.style.height = '500px'
-        menu.style.left = '0px'
-        menu.style.opacity ='1'
+                menu.style.display = 'block'
+                menu.style.backgroundColor = 'red'
+
+                i1.style.display = 'inline-block'
+                i2.style.display = 'inline-block'
+                i3.style.display = 'inline-block'
+                i4.style.display = 'inline-block'
+
+                menu.style.position = 'absolute'
+                menu.style.width = '100px'
+                menu.style.height = '450px'
+                menu.style.left = '0px'
+                menu.style.opacity ='1'
+        }
+        else{
+                menu.style.display = 'none'
+                imenu.style.backgroundColor = 'rgb(22, 22, 22)'
+                imenu.style.borderRadius = 'none'
+                imenu.style.padding = '0px'
+        }
 }
 
+function clicou1(){
+        i1.style.backgroundColor = 'yellow'
+        i2.style.backgroundColor = 'red'
+        i3.style.backgroundColor = 'red'
+        i4.style.backgroundColor = 'red'
+        if(secHam.style.display == 'none'){
+                secPizza.style.display = 'none'
+                secBebidas.style.display = 'none'
+                secSorvetes.style.display = 'none'
+                secHam.style.display = 'block'
+        }
+}
+
+function clicou2(){
+        i1.style.backgroundColor = 'red'
+        i2.style.backgroundColor = 'yellow'
+        i3.style.backgroundColor = 'red'
+        i4.style.backgroundColor = 'red'
+        if(secPizza.style.display == 'none'){
+                secHam.style.display = 'none'
+                secBebidas.style.display = 'none'
+                secSorvetes.style.display = 'none'
+                secPizza.style.display = 'block'
+        }
+}
+
+function clicou3(){
+        i1.style.backgroundColor = 'red'
+        i2.style.backgroundColor = 'red'
+        i3.style.backgroundColor = 'yellow'
+        i4.style.backgroundColor = 'red'
+        if(secBebidas.style.display == 'none'){
+                secHam.style.display = 'none'
+                secPizza.style.display = 'none'
+                secSorvetes.style.display = 'none'
+                secBebidas.style.display = 'block'
+        }
+}
+
+function clicou4(){
+        i1.style.backgroundColor = 'red'
+        i2.style.backgroundColor = 'red'
+        i3.style.backgroundColor = 'red'
+        i4.style.backgroundColor = 'yellow'
+        if(secSorvetes.style.display == 'none'){
+                secHam.style.display = 'none'
+                secPizza.style.display = 'none'
+                secBebidas.style.display = 'none'
+                secSorvetes.style.display = 'block'
+        }
+}
 
 //function entrou(){
 //    menu.style.backgroundColor = 'red'
