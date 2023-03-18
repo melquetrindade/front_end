accd4d5669c15ff2ed281120999cde15c0b060f2
melquetrindade <melquetrindade654@gmail.com>
2023-03-18 11:24:26 -0300

    finalizando os modos de exibição do relógio

--- proj_relogio/js/script.js
+++ proj_relogio/js/script.js
@@ -11,7 +11,8 @@ var segundos = 0
 
 var dispara = true
 var altoriza = true
-let interval = 0
+let interval24 = 0
+let intervalAm = 0
 
 function atualiza(){
 
@@ -36,7 +37,7 @@ if(dispara == true){
 }
 
 function disparaRel(){
-    interval = setInterval(() => {
+    interval24 = setInterval(() => {
         segundos += 1
         if(segundos === 60){
             minutos += 1
@@ -59,6 +60,47 @@ function disparaRel(){
     },1000)
 }
 
+
+function modoAm(){
+    intervalAm = setInterval(() =>{
+        segundos += 1
+        if(segundos == 60){
+            segundos = 0
+            minutos += 1
+        }
+        if(minutos == 60){
+            minutos = 0
+            hora += 1
+        }
+        if(hora == 12){
+            hora = 1
+            if(modoel.innerHTML === "AM"){
+                modoel.innerHTML = "PM"
+            }
+            else{
+                modoel.innerHTML = "AM"
+            }
+        }
+
+        horaEl.innerHTML = formataTime(hora)
+        minutosEl.innerHTML = formataTime(minutos)
+        segundosEl.innerHTML = formataTime(segundos)
+
+    },1000)
+}
+
+function transformaModelo(){
+    if(hora < 12){
+        return
+    }
+    else if(hora == 12){
+        hora = 0
+    }
+    else{
+        hora = hora - 12
+    }
+}
+
 function formataTime(time){
     if(time < 10){
         return `0${time}`
@@ -69,16 +111,27 @@ function formataTime(time){
 }
 
 comAmEl.addEventListener("change", function(){
-    modoel.innerHTML = "Modo AM/PM"
     dispara = false
-    clearInterval(interval)
+    clearInterval(interval24)
+    clearInterval(intervalAm)
+    atualiza()
+    if(hora < 12){
+        modoel.innerHTML = "AM"
+    }
+    else{
+        modoel.innerHTML = "PM"
+    }
+    transformaModelo()
+    modoAm()
+    
 
 })
 
 semAmEl.addEventListener("change", function(){
     modoel.innerHTML = "Modo 24 horas"
     dispara = false
-    clearInterval(interval)
+    clearInterval(interval24)
+    clearInterval(intervalAm)
     atualiza()
     disparaRel()
 })
