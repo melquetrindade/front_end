e54ff173c3238ebde5bf73cd95ea652fc430e725
melquetrindade <melquetrindade654@gmail.com>
2023-03-28 11:14:45 -0300

    finalizando a parte lógica e iniciando a media-queries

--- proj_CEP/js/script.js
+++ proj_CEP/js/script.js
@@ -2,6 +2,12 @@ const inputEl = document.querySelector("#cepId")
 const buscarEl = document.querySelector("#buscarBtn")
 const contFadeEl = document.querySelector("#fade")
 const loaderEl = document.querySelector("#loader")
+const contMsgEl = document.querySelector("#mensagem")
+const msgText = document.querySelector("#mensagem p")
+const cidadeEl = document.querySelector("#cityId")
+const regionEl = document.querySelector("#regionId")
+const btnClose = document.querySelector("#close-message")
+const btnLimpar = document.querySelector("#limparBtn")
 
 inputEl.addEventListener("keypress", (e) => {
     const onlyNumbers = /[0-9]/
@@ -31,6 +37,19 @@ async function processCEP(cep){
     const data = await resposta.json()
     console.log(data)
 
+    if(data.erro === true){
+        toggleLoader()
+        inputEl.value = ""
+        toggleMenssage("CEP inválido, tente novamente!")
+        return
+    }
+
+    cidadeEl.value = data.localidade
+    cidadeEl.style.color = "white"
+    regionEl.value = data.uf
+    regionEl.style.color = "white"
+    inputEl.value = ""
+
     toggleLoader()
 }
 
@@ -38,3 +57,21 @@ function toggleLoader(){
     contFadeEl.classList.toggle("hide")
     loaderEl.classList.toggle("hide")
 }
+
+function toggleMenssage(msg){
+    contFadeEl.classList.toggle("hide")
+    contMsgEl.classList.toggle("hide")
+    msgText.innerHTML = `${msg}`
+}
+
+btnClose.addEventListener("click", () => {
+    toggleMenssage("")
+})
+
+btnLimpar.addEventListener("click", () => {
+    inputEl.value = ""
+    cidadeEl.value = ""
+    regionEl.value = regionEl[0].value
+    cidadeEl.style.color = "var(--cor05)"
+    regionEl.style.color = "var(--cor05)"
+})
